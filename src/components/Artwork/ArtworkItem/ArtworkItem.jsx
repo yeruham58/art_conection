5cecbd77def579f0a9ef7b05db53b2e424908c06
Jeremy Landau <jeremylandau@MacBook-Pro.local>
2022-02-15 17:29:40 -0800

    eslint and prettier config

--- src/components/Artwork/ArtworkItem/ArtworkItem.jsx
+++ src/components/Artwork/ArtworkItem/ArtworkItem.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
-import { AddShoppingCart } from '@material-ui/icons';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
 
 import useStyles from './styles'
 
@@ -11,7 +18,11 @@ const ArtworkItem = ({ artwork, onAddToCart }) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={artwork.image.url} title={artwork.name} />
+      <CardMedia
+        className={classes.media}
+        image={artwork.image.url}
+        title={artwork.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
@@ -21,7 +32,12 @@ const ArtworkItem = ({ artwork, onAddToCart }) => {
             ${artwork.price.formatted}
           </Typography>
         </div>
-        <Typography dangerouslySetInnerHTML={{ __html: artwork.description }} variant="body2" color="textSecondary" component="p" />
+        <Typography
+          dangerouslySetInnerHTML={{ __html: artwork.description }}
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
         <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
@@ -33,4 +49,3 @@ const ArtworkItem = ({ artwork, onAddToCart }) => {
 }
 
 export default ArtworkItem
-
--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -1,26 +1,47 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+import { Link, useLocation } from 'react-router-dom'
 
-import logo from '../../assets/logo.png';
-import useStyles from './styles';
+import logo from '../../assets/logo.png'
+import useStyles from './styles'
 
 const PrimarySearchAppBar = ({ totalItems }) => {
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
-  const classes = useStyles();
-  const location = useLocation();
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null)
+  const classes = useStyles()
+  const location = useLocation()
 
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
 
-  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null)
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = 'primary-search-account-menu-mobile'
 
   const renderMobileMenu = (
-    <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
       <MenuItem>
-        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+        <IconButton
+          component={Link}
+          to="/cart"
+          aria-label="Show cart items"
+          color="inherit"
+        >
           <Badge badgeContent={totalItems} color="secondary">
             <ShoppingCart />
           </Badge>
@@ -28,30 +49,46 @@ const PrimarySearchAppBar = ({ totalItems }) => {
         <p>Cart</p>
       </MenuItem>
     </Menu>
-  );
+  )
 
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="artConection.js" height="70px" className={classes.image} /> 
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="artConection.js"
+              height="70px"
+              className={classes.image}
+            />
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
-          <div className={classes.button}>
-            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
           )}
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
     </>
-  );
-};
+  )
+}
 
-export default PrimarySearchAppBar;
+export default PrimarySearchAppBar
