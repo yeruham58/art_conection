c43ca6efb94da7c983685a2e2e9640952b351e88
Jeremy Landau <jeremylandau@MacBook-Pro.local>
2022-05-11 14:48:26 -0700

    more home page

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import { Card, Typography, Divider } from '@material-ui/core'
+import { Card } from '@material-ui/core'
 
 import { theme } from '../../utils/theme'
 
 import blackBackground from '../../assets/iconBlackBackground.png'
+import yellowBackground from '../../assets/background-2.jpg'
+import { paths } from '../../utils/paths'
 
 import { ArtworkList, ArtConnectionList, ArtistList } from '..'
 import SectionTitle from './SeftionTitle'
@@ -28,12 +31,12 @@ const HomePage = ({ onAddToCart, isMobile }: Props) => {
           <div
             style={{
               color: theme.color.red,
-              margin: isMobile ? 12 : 30,
+              margin: isMobile ? 16 : 30,
               fontFamily: 'cursive',
               fontSize: isMobile ? 30 : 60,
             }}
           >
-            Let art take the world one step forward
+            Let art take the world - <br /> one step forward!
           </div>
           <img
             alt="hulfLogo"
@@ -48,10 +51,79 @@ const HomePage = ({ onAddToCart, isMobile }: Props) => {
 
       <SectionTitle isMobile={isMobile} title="Our Projects" />
       <ArtConnectionList />
+
+      <Card style={{ marginTop: 24 }}>
+        <div
+          style={{
+            backgroundImage: `url(${yellowBackground})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        >
+          <div
+            style={{
+              minHeight: isMobile ? 300 : 400,
+              minWidth: isMobile ? 200 : 500,
+              paddingLeft: '60%',
+              paddingTop: 15,
+              textAlign: 'center',
+              fontFamily: 'cursive',
+              fontSize: isMobile ? 40 : 60,
+            }}
+          >
+            <div style={{ maxWidth: isMobile ? 100 : 180 }}>
+              <div>Proud to bee a part of it!!!</div>
+              <Link
+                to={paths.about}
+                style={{ textDecoration: 'none', color: 'black' }}
+              >
+                <div
+                  style={{
+                    backgroundColor: 'white',
+                    border: 1,
+                    borderRadius: '25px',
+                    fontSize: 12,
+                    fontFamily: 'serif',
+                    cursor: 'pointer',
+                    margin: 10,
+                  }}
+                >
+                  More about us
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Card>
+
       <SectionTitle isMobile={isMobile} title="Our artworks" />
       <ArtworkList onAddToCart={onAddToCart} />
+      <Card style={{ marginTop: 24 }}>
+        <div
+          style={{
+            backgroundColor: theme.color.blue,
+            color: theme.color.yellow,
+            textAlign: 'center',
+            padding: 50,
+            fontSize: isMobile ? 20 : 60,
+            fontFamily: 'Fantasy',
+          }}
+        >
+          <strong>
+            "Alone we can do so little, <br /> together we can do so much."
+          </strong>
+          <div style={{ fontSize: 16, margin: 24 }}>Helen Keller</div>
+        </div>
+      </Card>
       <SectionTitle isMobile={isMobile} title="Our artists" />
       <ArtistList />
+      <Card style={{ backgroundColor: 'black', marginTop: 24 }}>
+        <div style={{ color: 'white', margin: 20 }}>
+          <div style={{ marginTop: 10 }}>About us</div>
+          <div style={{ marginTop: 10 }}>Our return policy</div>
+          <div style={{ marginTop: 10 }}>Our colaberations</div>
+        </div>
+      </Card>
     </>
   )
 }
